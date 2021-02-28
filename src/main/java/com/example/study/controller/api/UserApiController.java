de3ccaaad9c1019a96fa86f128ba6c4a0783b835
ehj0529 <63188985+ehj0529@users.noreply.github.com>
2021-02-28 22:15:52 +0900

    item 에 대한 CRUD 적용 완료

--- src/main/java/com/example/study/controller/api/UserApiController.java
+++ src/main/java/com/example/study/controller/api/UserApiController.java
@@ -2,34 +2,44 @@ package com.example.study.controller.api;
 
 import com.example.study.ifs.CrudInterface;
 import com.example.study.model.network.Header;
+import com.example.study.model.network.request.UserApiRequest;
+import com.example.study.model.network.response.UserApiResponse;
+import com.example.study.service.UserApiLogicService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/user")
-public class UserApiController implements CrudInterface {
+public class UserApiController implements CrudInterface<UserApiRequest, UserApiResponse> {
 
+    @Autowired
+    private UserApiLogicService userApiLogicService;
 
     @Override
     @PostMapping("")  // 값을 넣지 않으면 /api/user 로 매핑.
-    public Header create() {
-        return null;
+    public Header<UserApiResponse> create(@RequestBody Header<UserApiRequest> request) {
+        log.info("{}",request );
+        return userApiLogicService.create(request);
     }
 
     @Override
     @GetMapping("{id}")  // /api/user/{id} 로 호출된다.
-    public Header read(@PathVariable(name = "id") Long id) {
-        return null;
+    public Header<UserApiResponse> read(@PathVariable(name = "id") Long id) {
+        log.info("read id : {}",id);
+        return userApiLogicService.read(id);
     }
 
     @Override
     @PutMapping("") // /api/user 로 호출
-    public Header update() {
-        return null;
+    public Header<UserApiResponse> update(@RequestBody Header<UserApiRequest> request) {
+        return userApiLogicService.update(request);
     }
 
     @Override
     @DeleteMapping("{id}") // /api/user/{id}
     public Header delete(@PathVariable(name = "id") Long id) {
-        return null;
+        return userApiLogicService.delete(id);
     }
 }
