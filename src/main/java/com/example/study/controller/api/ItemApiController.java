c45a08e8b9a98a1b5e06a8e2186db6fb251c31a5
ehj0529 <63188985+ehj0529@users.noreply.github.com>
2021-03-02 22:48:06 +0900

    controll 추상화 적용

--- src/main/java/com/example/study/controller/api/ItemApiController.java
+++ src/main/java/com/example/study/controller/api/ItemApiController.java
@@ -1,5 +1,6 @@
 package com.example.study.controller.api;
 
+import com.example.study.controller.CrudController;
 import com.example.study.ifs.CrudInterface;
 import com.example.study.model.network.Header;
 import com.example.study.model.network.request.ItemApiRequest;
@@ -8,13 +9,23 @@ import com.example.study.service.ItemApiLogicService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.PostConstruct;
+
 @RestController
 @RequestMapping("/api/item")
-public class ItemApiController implements CrudInterface<ItemApiRequest, ItemApiResponse> {
+public class ItemApiController //implements CrudInterface<ItemApiRequest, ItemApiResponse> { // interface 상속을 제거
+         extends CrudController<ItemApiRequest, ItemApiResponse>{  // 인터페이스 대신 추상화 클래스 상속으로 아래의 메소드를 삭제 할수 있음.
 
     @Autowired
     private ItemApiLogicService itemApiLogicService;
 
+    // 추상화 클래스의 baseService 에 대한 정의가 필요함
+    @PostConstruct
+    public void init(){
+        this.baseService = itemApiLogicService; //
+    }
+/*
+     동일한 메소드가 사용됨으로 관련 된 내용을 abstract class 로 대체 하면 아래의 메소드를 생략 할수 있다.
     @Override
     @PostMapping("") // /api/item
     public Header<ItemApiResponse> create(@RequestBody  Header<ItemApiRequest> request) {
@@ -38,4 +49,6 @@ public class ItemApiController implements CrudInterface<ItemApiRequest, ItemApiR
     public Header delete(@PathVariable Long id) {
         return itemApiLogicService.delete(id);
     }
+
+ */
 }
